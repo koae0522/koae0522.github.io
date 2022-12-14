46ce0b3779fa08f4d5f24dd6d1898debe74a1cb6
koae0522 <yantukeuma@gmail.com>
2022-12-14 23:17:48 +0900

    あ

--- contents/bingo/bingo.js
+++ contents/bingo/bingo.js
@@ -1,56 +1,94 @@
-//何回目か
 var nankai = 0;
 var first = true;
 
-var Nary = [];
+var ary = [];
+var Aary = [];
+
 var Bary = [];
+var Iary = [];
+var Nary = [];
+var Gary = [];
+var Oary = [];
 
 function S() {
-    if (first == true) {
-        Start();
-    }
-    else {
-        Turn();
-    }
+  if (first == true) {
+    Start();
+  } else {
+    Turn();
+  }
 }
 
 function Start() {
-    var i = 0;
+  console.log("start");
+  first = false;
+  var i = 0;
 
-    for (i = 0; i < 75; i++) {
-        Nary[i] = i + 1;
-    }
+  for (i = 0; i < 75; i++) {
+    ary[i] = i + 1;
+  }
 
-    for (var i = (Nary.length - 1); 0 < i; i--) {
-        var r = Math.floor(Math.random() * (i + 1));
-        var tmp = Nary[i];
-        Nary[i] = Nary[r];
-        Nary[r] = tmp;
-    }
-    
-    nankai++;
-    var n = document.getElementById("num").innerText = Nary[nankai - 1];
-    console.log(n);
-    Bary.push(n);
-    document.getElementById("imamade").innerText = Bary;
-    first = false;
+  console.log(ary);
+
+  for (var i = ary.length - 1; 0 < i; i--) {
+    var r = Math.floor(Math.random() * (i + 1));
+    var tmp = ary[i];
+    ary[i] = ary[r];
+    ary[r] = tmp;
+  }
+
+  console.log(ary);
+  console.log(first);
+  Turn();
 }
 
 function Turn() {
-    nankai++;
-    var n = document.getElementById("num").innerText = Nary[nankai - 1];
-    console.log(n);
+  console.log("turn");
+  nankai++;
+  var n = (document.getElementById("num").innerText = ary[nankai - 1]);
+  console.log(n);
+  Aary.push(n);
+  Aary.sort(function (a, b) {
+    if (a < b) return -1;
+    if (a > b) return 1;
+    return 0;
+  });
+
+  if (n < 15) {
     Bary.push(n);
-    Bary.sort(function(a,b)
-    {
-        if( a < b ) return -1;
-        if( a > b ) return 1;
-        return 0;
+    Bary.sort(function (a, b) {
+      if (a < b) return -1;
+      if (a > b) return 1;
+      return 0;
     });
-    document.getElementById("imamade").innerText = Bary;
+    document.getElementById("b").innerText = Bary;
+  } else if (n < 30) {
+    Iary.push(n);
+    Iary.sort();
+    document.getElementById("i").innerText = Iary;
+  } else if (n < 45) {
+    Nary.push(n);
+    Nary.sort();
+    document.getElementById("n").innerText = Nary;
+  } else if (n < 60) {
+    Gary.push(n);
+    Gary.sort();
+    document.getElementById("g").innerText = Gary;
+  } else if (n < 75) {
+    Oary.push(n);
+    Oary.sort();
+    document.getElementById("o").innerText = Oary;
+  }
+
+  console.log(Bary);
+  console.log(Iary);
+  console.log(Nary);
+  console.log(Gary);
+  console.log(Oary);
+
+  document.getElementById("imamade").innerText = Aary;
 }
 
-function Name()
-{
-    document.getElementById("title").innerText=document.getElementById("name").value;
+function Name() {
+  document.getElementById("title").innerText =
+    document.getElementById("name").value;
 }
