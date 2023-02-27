a50d12ca53cc4a3eda91badcf37375cf6b73b2bc
koae0522 <yantukeuma@gmail.com>
2023-02-27 20:55:22 +0900

    take

--- contents/banbooSimulator/banboo.js
+++ contents/banbooSimulator/banboo.js
@@ -8,6 +8,14 @@
     }
     else
     {
+        bamboo();
+        //一分ごとに更新
+        setInterval("bamboo()", 60000);
+    }
+
+    function bamboo()
+    {
+        //高さ計算
         console.log("はじめてじゃない");
         console.log("最初時刻"+localStorage.firstTime);
         currentTime=Math.floor(nowTime.getTime()/(1000*60));
@@ -16,11 +24,40 @@
         let sa=currentTime-localStorage.firstTime;
         console.log("差     "+sa);
 
-        let currentHeight=(sa/60).toFixed(2);
-        console.log(currentHeight);
+        localStorage.currentHeight=(sa/60).toFixed(2);
+        console.log(localStorage.currentHeight);
         let banbooHeight=document.getElementById("banbooHeight");
-        banbooHeight.innerHTML=currentHeight+"cm";
 
+        //テスト用強制成長コード
+        //localStorage.currentHeight=10;
+
+        //高さ表示
+        if(localStorage.currentHeight<100)
+        {
+            banbooHeight.innerHTML=localStorage.currentHeight+"cm";
+        }
+        else
+        {
+            banbooHeight.innerHTML=Math.floor(localStorage.currentHeight/100)+"m"
+            +(localStorage.currentHeight % 100)+"cm";
+        }
+        
+        //画像表示
+        if(localStorage.currentHeight<50)
+        {
+            let heightPx=(localStorage.currentHeight);
+            let imgElm=document.getElementById("img");
+            imgElm.style.width=heightPx+"%";
+        }
+        else
+        {
+            let heightPx=localStorage.currentHeight;
+            let imgElm=document.getElementById("img");
+            imgElm.src="image/bamboo.png";
+            imgElm.style.width="100%";
+            imgElm.style.height=heightPx+"px";
+        }
+           
     }
 
 
