3b8c569ef606982a5f7d2f101415553e1e42a37b
koae0522 <yantukeuma@gmail.com>
2023-03-03 11:41:04 +0900

    deketa

--- contents/banbooSimulator/banboo.js
+++ contents/banbooSimulator/banboo.js
@@ -17,20 +17,20 @@
     function bamboo()
     {
         //高さ計算
-        console.log("はじめてじゃない");
-        console.log("最初時刻"+localStorage.firstTime);
+       // console.log("はじめてじゃない");
+       // console.log("最初時刻"+localStorage.firstTime);
         currentTime=Math.floor(nowTime.getTime()/(1000*60));
-        console.log("現在時刻"+currentTime);
+        //console.log("現在時刻"+currentTime);
         
         let sa=currentTime-localStorage.firstTime;
-        console.log("差     "+sa);
+       // console.log("差     "+sa);
 
         localStorage.currentHeight=(sa/60).toFixed(2);
-        console.log(localStorage.currentHeight);
+      //  console.log(localStorage.currentHeight);
         let banbooHeight=document.getElementById("banbooHeight");
 
         //テスト用強制成長コード
-        //localStorage.currentHeight=100;
+        //localStorage.currentHeight=50;
 
         //高さ表示
         if(localStorage.currentHeight<100)
@@ -63,7 +63,7 @@
         time=nowTime.getHours();
         let textElm=document.getElementById("text");
         let bodyElm=document.body;
-        time=16;
+
 
         //夜 19~5
         if(time>=19||time<5)
@@ -91,8 +91,11 @@
         }
     }
 
-    function tweet(){
-        let text="私の竹は"+document.getElementById("banbooHeight").innerHTML+"になりました";
+
+    let tweetbtn=document.getElementById("tweet");
+    tweetbtn.addEventListener("click",()=>{
+        let text="私の竹は"+document.getElementById("banbooHeight").innerHTML+"になりました"+"koae0522.github.io/contents/banbooSimulator/banboo.html";
         window.open("https://twitter.com/share?text="+text);
-    }
+    });
+       
 
