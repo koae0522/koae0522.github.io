72c6b749abc6c58ff5f9737d81189c9ece803f64
koae0522 <yantukeuma@gmail.com>
2023-04-28 21:59:43 +0900

    ばぐとった

--- contents/siwake/main.js
+++ contents/siwake/main.js
@@ -4,9 +4,10 @@ phina.globalize();
 //サイズ指定用の定数
 let SCREEN_X = 900;
 let SCREEN_Y = 600;
+
 let scoreLabel;
 let score=0;
-let interval=3;
+let interval=2;
 let previousTime=0;
 
 var blueCat={
@@ -21,8 +22,8 @@ var blueCat={
 // MainScene クラスを定義
 phina.define('MainScene', {
     superClass: 'DisplayScene',
-    init: function(option) {    //ここにoptionを追加
-      this.superInit(option);   //こっちにも
+    init: function(option) {    
+      this.superInit(option);   
 
       // 背景色を指定
       this.backgroundColor = '#444';
@@ -39,8 +40,8 @@ phina.define('MainScene', {
 
       if((gameTime-previousTime)>interval||interval<=0.3&&(gameTime-previousTime)>interval){
         previousTime=gameTime;
-        if(interval>0.3){
-          interval-=0.3;
+        if(interval>0.1){
+          interval-=0.5;
         }
         let ran=Math.randint(0,1);
         let color;
@@ -106,7 +107,7 @@ phina.define('MainScene', {
           }
           else if(this.scaleX<0){
             this.scaleX+=0.005;
-            this.scaleY+=0.005;
+            this.scaleY-=0.005;
           }
           else if(this.scaleX==0){
             this.hide();
@@ -121,45 +122,56 @@ phina.define('MainScene', {
       //不正解
       if(this.color=="blue"&&this.x>=600||this.color=="pink"&&this.x<=300){
         this.update=function(){
-          if(this.scaleX!=0){
+          if(this.scaleX>0){
             this.scaleX+=0.5;
             this.scaleY+=0.5;
           }
+          else if(this.scaleX<0){
+            this.scaleX-=0.5;
+            this.scaleY+=0.5;
+          }
+          else if(this.scaleX==0){
+            this.hide();
+          }
         
         }
-
+        console.log("不正解");
       }
      },
      
      //ずっと繰り返し
      update:function(app){
+      //マウスに追従
       if(this.pointed==true){
         this.x = app.pointer.x; 
         this.y = app.pointer.y; 
       }
-      //動かす
+      //マウスでポイントされてないとき動かす
       else if(this.pointed==false&&(this.x>=320||this.x<=580||this.y<=580||this.y>=20)){
         this.x+=this.dirX;
         this.y+=this.dirY;
-        if(this.x<=320||this.x>=580||this.y>=580||this.y<=20){
+
+        //跳ね返ったとき
+        if(this.x<=320||this.x>=580){
           this.dirX=-this.dirX;
-          this.dirY=-this.dirY;
           this.scaleX *= -1;
+        }
+        else if(this.y>=580||this.y<=20){
+            this.dirY=-this.dirY;
+        }
+     
         }
       }
      },
-  });
+  );
 
-// メイン処理
+// メイン
 phina.main(function() {
-  // アプリケーション生成
   var app = GameApp({
-    startLabel: 'main', // メインシーンから開始する
-    //ここでscreenのサイズを変更 ここはOK
+    startLabel: 'main', 
     width: SCREEN_X,
     height: SCREEN_Y,
     assets: blueCat,
   });
-  // アプリケーション実行
   app.run();
 });
